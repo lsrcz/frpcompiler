567d2ffdbb435fceda1f2a97450c181caa9ae404
Sirui Lu <lsrcz@pku.edu.cn>
2019-08-26 06:05:22 -0700

    nested ref

--- ast/find-missing.rkt
+++ ast/find-missing.rkt
@@ -1,7 +1,8 @@
 #lang racket
 
 (require "../util/util.rkt")
-(require "../ast/syntax.rkt")
+(require "syntax.rkt")
+(require "nested-ref.rkt")
 
 (provide
  find-missing-var
@@ -24,7 +25,7 @@
       (merge-two (car missing-lst) (merge-missing (cdr missing-lst)))))
 
 
-(define (find-missing-var lst funclst return-val arg)
+(define (find-missing-var lst funclst return-val ref-list arg)
   (define (find-missing-var-inner lst arg)
     (let ([index (index-of lst arg)])
       (if index
@@ -34,18 +35,18 @@
                                    (for/list ([a arg])
                                      (find-missing-var-inner lst a))))
               (let ([removed (remove-prev arg)])
-                (if (eq? return-val removed)
+                (if (or (eq? return-val removed) (resolve-ref removed ref-list))
                     (all-found)
                     (not-found (list removed))))))))
   (match (find-missing-var-inner (append lst funclst) arg)
     [(all-found) (all-found)]
     [(not-found lst) (not-found (remove-duplicates lst))]))
 
-(define (find-missing-var-deep lst funclst return-val inst)
+(define (find-missing-var-deep lst funclst return-val ref-list inst)
   (let* ([missing-in-arg
           (if (custom? inst)
               (all-found)
-              (find-missing-var lst funclst return-val
+              (find-missing-var lst funclst return-val ref-list
                                 ((cond [(bind? inst) bind-body]
                                        [(if? inst) if-arg]
                                        [(if-else? inst) if-else-arg]
@@ -54,7 +55,7 @@
          [new-lst (append (if (bind? inst) (list (bind-name inst)) '()) lst)]
          [missing-in-next-inst-first
           (if (not (return? inst))
-              (find-missing-var-deep new-lst funclst return-val
+              (find-missing-var-deep new-lst funclst return-val ref-list
                                      ((cond [(bind? inst) bind-inst]
                                             [(if? inst) if-branch]
                                             [(if-else? inst) if-else-then-branch]
@@ -64,11 +65,15 @@
               (all-found))]
          [missing-in-next-inst-second
           (if (if-else? inst)
-              (find-missing-var-deep new-lst funclst return-val (if-else-else-branch inst))
+              (find-missing-var-deep new-lst funclst return-val ref-list (if-else-else-branch inst))
               (all-found))])
     (merge-missing (list missing-in-arg missing-in-next-inst-first missing-in-next-inst-second))))
 
 (define (main)
-  (println (find-missing-var '((g a)) '(f g) 'd '(f (g a))))
-  (println (find-missing-var '() '(f g) 'd '(f (g (prev a)))))
-  (println (find-missing-var-deep '() '(f g) 'd '(bind a (f t) (return a)))))
+  (println (find-missing-var '((g a)) '(f g) 'd '() '(f (g a))))
+  (println (find-missing-var '() '(f g) 'd '() '(f (g (prev a)))))
+  (println (find-missing-var-deep '() '(f g) 'd '() '(bind a (f t) (return a))))
+  (println (find-missing-var-deep '() '(f g) 'd (list (nested-ref-table 't '())) '(bind a (f t) (return a))))
+  (println (find-missing-var-deep '() '(f g) 'd (list (nested-ref-table 't '())) '(bind a (f t) (bind b (g s) (return a)))))
+  (println (find-missing-var-deep '() '(f g) 'd (list (nested-ref-table 't '(s))) '(bind a (f t) (bind b (g s) (return a)))))
+  )
