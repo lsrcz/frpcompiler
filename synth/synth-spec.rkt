1e53ca76afa568def308b50426ecacc526aa72ee
Sirui Lu <lsrcz@pku.edu.cn>
2019-10-10 23:54:43 +0800

    synth

--- synth/synth-spec.rkt
+++ synth/synth-spec.rkt
@@ -4,7 +4,6 @@
 (require "../interpret/interpret-spec/spec.rkt")
 (require "../interpret/interpret-spec/environment.rkt")
 (require "../interpret/subscribe-fsm.rkt")
-(require rosette/lib/match)
 (require rosette/lib/angelic)
 
 (define sprinkler-spec
@@ -14,21 +13,21 @@
    '(eq? undefined?)
    '(six sixten detected not-detected on off f1 f2)
    '((clock six) (motion not-detected))
-   '((clock (case-multi ((eq? clock six))
-                         ;(eq? clock sixten)
-                         ;(and (eq? motion detected) (eq? (prev motion) not-detected))
-                         ;(and (eq? motion not-detected) (eq? (prev motion) detected)))
-                        ((return on))
-                         ;(return off)
-                         ;(return-empty))
+   '((clock (case-multi ((eq? clock six)
+                         (eq? clock sixten)
+                         (and (eq? motion detected) (eq? (prev motion) not-detected))
+                         (and (eq? motion not-detected) (eq? (prev motion) detected)))
+                        ((return on)
+                         (return off)
+                         (return-empty))
                         f1))
-     (motion (case-multi ((eq? clock six))
-                          ;(eq? clock sixten)
-                          ;(and (eq? motion detected) (eq? (prev motion) not-detected))
-                          ;(and (eq? motion not-detected) (eq? (prev motion) detected)))
-                         ((return on))
-                          ;(return off)
-                          ;(return-empty))
+     (motion (case-multi ((eq? clock six)
+                          (eq? clock sixten)
+                          (and (eq? motion detected) (eq? (prev motion) not-detected))
+                          (and (eq? motion not-detected) (eq? (prev motion) detected)))
+                         ((return on)
+                          (return off)
+                          (return-empty))
                          f2)))))
 
 (define six 'six)
@@ -38,11 +37,7 @@
 (define on 'on)
 (define off 'off)
 
-(define (f1 b)
-  0)
-(define (f2 b)
-  0)
-#|
+
 (define (f1 b1 b2 b3 b4)
   (cond [b1 0]
         [b2 1]
@@ -50,7 +45,7 @@
 (define (f2 b1 b2 b3 b4)
   (cond [(and b4 b1) 0]
         [b3 1]
-        [else 2]))|#
+        [else 2]))
 
 (define (and-func a b)
   (and a b))
@@ -107,7 +102,7 @@
 
 (define stream-body-for-testing-analyze (spec-body sprinkler-spec))
 
-(interpret-spec sprinkler-spec trace-input bindings)
+(eq? trace-result (interpret-spec sprinkler-spec trace-input bindings))
 
 
 
@@ -124,11 +119,11 @@
 (define (get-symbolic-event constructor-mapping)
   (if (null? constructor-mapping)
       (empty-event)
-      (let ([cur (car constructor-mapping)]
-            [rest (cdr constructor-mapping)])
-        (match cur
-          [(cons name constructor)
-           (choose* (get-single-symbolic-event name constructor) (get-symbolic-event rest))]))))
+      (let* ([cur (car constructor-mapping)]
+             [rest (cdr constructor-mapping)]
+             [name (car cur)]
+             [constructor (cdr cur)])
+        (choose* (get-single-symbolic-event name constructor) (get-symbolic-event rest)))))
 
 (define (get-symbolic-event-list constructor-mapping length)
   (define (iter num)
@@ -147,12 +142,6 @@
 
 (define model (synthesize #:forall '()
                           #:guarantee (assert (equal? trace-input sym-trace))))
-(evaluate sym-trace model)
-(displayln "BUGGY???????")
-
-(interpret-spec sprinkler-spec (evaluate sym-trace model) bindings)
-(evaluate (interpret-spec sprinkler-spec sym-trace bindings) model)
-(car '())
 
 (define m1 (time (synthesize #:forall '()
                        #:guarantee (begin
@@ -166,26 +155,30 @@
 (define-symbolic b2 boolean?)
 (define-symbolic b3 boolean?)
 (define-symbolic b4 boolean?)
+#|
 (f1c b1 b2 b3 b4)
 (f1c1 b1 b2 b3 b4)
 (evaluate sym-trace m1)
+(evaluate (interpret-spec sprinkler-spec sym-trace bindings) m1)
 (evaluate (interpret-spec sprinkler-spec sym-trace symbolic-bindings) m1)
-
+(car '())
+|#
 
 (define (synthesize-spec spec bindings sym-trace init-trace init-result annotate-func)
   (define (iter inout last-model)
     (define (ground-truth-constraint inout)
-      (match inout
-        [(list) (void)]
-        [(cons (cons i o) rest)
-         (displayln o)
-         (begin
-           (assert (equal? (interpret-spec spec i bindings) o))
-           (ground-truth-constraint rest))]))
+      (if (null? inout)
+          (void)
+          (let* ([cur (car inout)]
+                 [i (car cur)]
+                 [o (cdr cur)]
+                 [rest (cdr inout)])
+            (begin
+              (assert (equal? (interpret-spec spec i bindings) o))
+              (ground-truth-constraint rest)))))
     (define (distinguish-constraint)
       (assert (not (equal? (interpret-spec spec sym-trace (evaluate bindings last-model))
                            (interpret-spec spec sym-trace bindings)))))
-    (displayln (interpret-spec spec sym-trace bindings))
     (define new-model (time (synthesize #:forall '()
                                         #:guarantee (begin
                                                       (ground-truth-constraint inout)
@@ -196,6 +189,10 @@
         (let* ([complete-model (complete-solution new-model (symbolics (cons sym-trace bindings)))]
                [in (evaluate sym-trace complete-model)]
                [out (annotate-func in)])
+          (displayln "NEW TRACE")
+          (displayln in)
+          (displayln out)
+          (displayln (evaluate (interpret-spec sprinkler-spec sym-trace bindings) complete-model))
           (iter (cons (cons in out) inout) complete-model))))
   (let* ([initial-model (complete-solution
                          (synthesize #:forall '()
@@ -208,9 +205,18 @@
   (let ([lst (read)])
     (map (lambda (x) (if (eq? x 'no-evt) (empty-event) (event 'sprinkler x))) lst)))
 
-;(synthesize-spec sprinkler-spec symbolic-bindings sym-trace annotate-func)
-(displayln sym-trace)
-(displayln (interpret-spec sprinkler-spec sym-trace symbolic-bindings))
+(define (annotate-groundtruth input-spec input-bindings)
+  (define (annotate-func in)
+    (interpret-spec input-spec in input-bindings))
+  annotate-func)
+
+(define synth-m (synthesize-spec sprinkler-spec symbolic-bindings sym-trace (trace (list)) (list)
+                                 (annotate-groundtruth sprinkler-spec bindings)))
+
+(verify (eq? (interpret-spec sprinkler-spec sym-trace (evaluate symbolic-bindings synth-m))
+             (interpret-spec sprinkler-spec sym-trace bindings)))
+
+
 
 
                                          
